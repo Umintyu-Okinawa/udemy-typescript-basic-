6490b8249cd2281e23acf2317a855119f5010bb9
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-03 11:59:25 +0900

    基本的な演算子を使おう

---
+++ section3/23/app.ts
@@ -0,0 +1,60 @@
+//足し算
+const ans = 1+2 ;
+console.log(ans);
+
+const i =20;
+const j =10;
+const ans1 = i+j;
+console.log (ans1);
+
+//引き算
+const ans2 = i-j;
+console.log(ans2);
+
+//掛け算
+const ans3 = i*j;
+console.log(ans3);
+
+//割り算
+const ans4 =i/j;
+console.log(ans4);
+
+const x =3;
+const y =2;
+const ans5 =x/y;
+console.log(ans5);
+
+//余り
+const ans6 = x%y;
+console.log(ans6);
+
+//べき乗
+
+const ans7 = x**y;
+console .log(ans7);
+
+const ans8 =x*x;
+console .log (ans8);
+
+let num3 = 20;
+num3 =num3 + 10;
+console.log(num3);
+
+let num4 = 20;
+num4 += 10;
+console.log(num4);
+
+//引き算
+let num5 = 20;
+num5 -= 10;
+console.log(num5);
+
+//掛け算
+let num6 = 20;
+num6 *= 10;
+console.log(num6);
+
+//割り算
+let num7 = 20;
+num7 /= 10;
+console.log(num7);
