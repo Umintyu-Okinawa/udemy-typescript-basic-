784360194292d8156a6d099d844917370889277f
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-03 18:00:15 +0900

    アロー関数を使おう

---
+++ section6/section6/50/app.ts
@@ -0,0 +1,15 @@
+// const getRectangleExpr = function(height: number, witdh: number) {
+   //  return height * witdh;
+//  };
+
+const getRectangle = ( height: number , witdh: number ) : number => {
+return height * with ;
+};
+//console.log(getRectangle(3,5));
+// console.log(rectangle('abc',5));
+
+//const getRectangle = ( height: number , witdh: number ) : number => height * with;
+//console.log(getRectangle(3,5));
+
+const hello = () => console.log('Hello');
+hello();
