574113a04c38d5a145f49d75c643079c5749ad92
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-03 11:59:43 +0900

    複合代入演算子を使おう

---
+++ section3/24/app.ts
@@ -0,0 +1,22 @@
+let num3 = 20;
+num3 =num3 + 10;
+console.log(num3);
+
+let num4 = 20;
+num4 += 10;
+console.log(num4);
+
+//引き算
+let num5 = 20;
+num5 -= 10;
+console.log(num5);
+
+//掛け算
+let num6 = 20;
+num6 *= 10;
+console.log(num6);
+
+//割り算
+let num7 = 20;
+num7 /= 10;
+console.log(num7);
