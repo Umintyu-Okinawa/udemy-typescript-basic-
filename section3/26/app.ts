152e98f0053a9c0b8df28c2efd185a2551ce221c
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-03 12:00:39 +0900

    文字列結合とテンプレートリテラルを使おう

---
+++ section3/26/app.ts
@@ -0,0 +1,7 @@
+const firstName = 'Yuta';
+const lastName = 'Nakamura';
+const message1 = 'Hello! I am' + firstName +'' + lastName + '.';
+console.log(message1);
+
+const message2 = `Hello! I am ${firstName}${lastName}.`;
+console.log(message2);
