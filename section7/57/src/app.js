0fb4ceef597b3938db465e873684ad18da553471
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 11:58:15 +0900

    オブジェクト(連想配列)を使おう

---
+++ section7/57/src/app.js
@@ -0,0 +1,43 @@
+// 例題：　ユーザーオブジェクトの操作
+//　変数userを用意
+//下記載のオブジェクトを作成
+//name:'Rion Nakamura'
+//gender:'man'
+//age25
+//各式(値)を、プロパティ(キー)を指定して、
+//取り出してコンソール表示
+// const user = {
+// name: 'Rion Nakamura',
+// gender: 'man',
+// age:21
+// };
+// console.log(user.name);
+// console.log(user.gender);
+// console.log(user.age);
+// console.log(user['name']);
+// console.log(user['gender']);
+// console.log(user['age']);
+// user.age = 26;
+// console.log(user.age);
+// user['age'] = 23;
+// console.log(user.age);
+// console.log(Object.keys(user).lenght);
+// console.log(Object.keys(user));
+var user2 = {
+    name: 'Yuta Nakamura',
+    gender: 'Man',
+    age: 20
+};
+//user2.age ='20'; //NG
+//suser2.prefecture ='Tokyo'; //NG
+console.log(user2.name);
+console.log(user2.gender);
+console.log(user2.age);
+var user3 = {
+    name: 'Yuta Nakamura',
+    gender: 'man',
+    age: 20
+};
+console.log(user3.name);
+console.log(user3.gender);
+console.log(user3.age);
