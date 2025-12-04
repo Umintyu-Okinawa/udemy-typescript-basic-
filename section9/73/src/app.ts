e110c0ac0be2252061caecbd8aaa21477441228f
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 14:12:21 +0900

    クラスの型を理解しよう

---
+++ section9/73/src/app.ts
@@ -0,0 +1,17 @@
+class User {
+  name: string = '';
+  private age: number = 0;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public isAdult(): boolean {
+    return this.age >= 18;
+  }
+
+}
+
+const olivia = new User('Olivia', 23);
+const liam: User = new User('Liam', 25);
