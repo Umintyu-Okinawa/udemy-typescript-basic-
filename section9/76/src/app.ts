d9396b8552900d3c66f3ddb682cad8290737ec22
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 14:24:06 +0900

    コンストラクタをオーバーライドしよう

---
+++ section9/76/src/app.ts
@@ -0,0 +1,36 @@
+class User {
+  name: string = '';
+  private age: number = 0;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public isAdult(): boolean {
+    return this.age >= 18;
+  }
+
+}
+
+class AdminUser extends User {
+  adminRole: number = 1;
+
+  constructor(name: string, age: number, adminRole: number) {
+    super(name, age);
+    this.adminRole = adminRole;
+  }
+
+  public sayAdminRole() {
+    console.log(`My adminRole is ${this.adminRole}.`);
+  }
+
+  public override isAdult(): boolean {
+    return true;
+  }
+}
+
+const emma = new AdminUser('Emma', 0, 2);
+console.log(emma.name);
+emma.sayAdminRole();
+console.log(emma.isAdult());
