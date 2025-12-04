417e0de37e4cc9474fc282638f763a642529b948
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 13:49:09 +0900

    オプショナルなプロパティを使おう

---
+++ section9/68/app.ts
@@ -0,0 +1,11 @@
+class User {
+  name?: string;
+  age: number = 0;
+}
+
+const olivia = new User();
+console.log(olivia.name);
+console.log(olivia.age);
+
+olivia.name = 'Olivia';
+console.log(olivia.name);
