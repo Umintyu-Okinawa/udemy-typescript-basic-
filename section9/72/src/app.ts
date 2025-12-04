5b0ca44372fa1f79dbdbdeaf0bf2fd6e737e2c17
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 14:07:57 +0900

    アクセシビリティ修復子を使おう

---
+++ section9/72/src/app.ts
@@ -0,0 +1,19 @@
+class User {
+  name: string = '';
+  private age: number = 0;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public isAdult(): boolean {
+    return this.age >= 18;
+  }
+
+}
+
+const olivia = new User('Olivia', 23);
+console.log(olivia.name);
+// console.log(olivia.age); // error
+console.log(olivia.isAdult());
