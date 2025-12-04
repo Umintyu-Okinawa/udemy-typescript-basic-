d0ab507e45f85c72308fc2e8b7eec52d54722511
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-04 10:53:04 +0900

    配列の基本を理解しよう

---
+++ section7/55/src/app.js
@@ -0,0 +1,14 @@
+//const colors : string [] = ['Red' , 'Green' , 'Blue'] ;
+//const colors : string [] = ['1' , '2' , '3'] ;//NG
+var colors = ['Red', 'Green', 'Blue'];
+console.log(colors);
+//const mixedArr = ['abc , 1, true,false];
+//condole.log(mixedArr);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+//console.log(colors[3]);//undifined
+console.log(colors.lenght);
+console.log(colors[colors.lenght - 1]);
+var emptyArr = [];
+console.log(emptyArr);
---
+++ section7/55/src/app.ts
@@ -0,0 +1,17 @@
+//const colors : string [] = ['Red' , 'Green' , 'Blue'] ;
+//const colors : string [] = ['1' , '2' , '3'] ;//NG
+const colors = ['Red' , 'Green' , 'Blue'] ;
+console.log(colors);
+
+//const mixedArr = ['abc , 1, true,false];
+//condole.log(mixedArr);
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+//console.log(colors[3]);//undifined
+console.log(colors.lenght);
+console.log(colors[colors.lenght - 1]);
+
+const emptyArr : number[] = [ ];
+console.log(emptyArr);
